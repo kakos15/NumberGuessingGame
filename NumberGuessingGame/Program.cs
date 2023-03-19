9c04d77049bfc64055d57f789b3a3893d29c3a94
Kyriakos Michael <kyriakos_15@yahoo.com>
2023-03-20 00:56:39 +0200

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+//Console.WriteLine("Hello, World!");
+
+Random r = new Random();
+int winNum = r.Next(0, 100);
+bool win = false;
+do {
+
+    Console.Write("Guess a number in between  0 and 100: ");
+    string s  = Console.ReadLine();
+
+    int i = int.Parse(s);
+    if (i > winNum)
+    {
+        Console.WriteLine("To high! Guess lower...");
+    }
+    else if (i < winNum)
+    {
+        Console.WriteLine("Go low! Guess higher...");
+    }
+    else if (i == winNum)
+    {
+        Console.WriteLine("YOU WIN");
+        win = true;
+    }
+}while(win ==false);
+Console.WriteLine("Thank you for playing the game");
+Console.WriteLine("Press any to finish ");
+Console.ReadKey(true);
+
+
